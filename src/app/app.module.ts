ad120a47beb84b3ca702bbd289f0c83abdc0e40d
AbiyHailu <abiyo02@yahoo.com>
2019-09-07 22:43:08 +0200

    5 files modified, 2 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: '', component: MainComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    MainComponent
   ],
   bootstrap: [ AppComponent ]
 })
