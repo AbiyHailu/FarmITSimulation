ae73e3053b7cc4fc58b74bbc0e062c85236179de
AbiyHailu <abiyo02@yahoo.com>
2019-09-10 21:22:31 +0200

    3 files modified

--- src/app/bottom-bar/bottom-bar.component.ts
+++ src/app/bottom-bar/bottom-bar.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BottomBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   this.getPlantAge()
+  }
 
   ngOnInit() {
   }
 
+  barValue = 0
+  count = 0
+  plantage = 12
+  interval 
+  getPlantAge() {
+    if (this.barValue <= this.plantage) {
+       this.interval  =  setInterval(() => {
+        this.barValue = this.barValue + 1
+        console.log('barValue', this.barValue)
+        this.changeProgressValue()
+         if (this.barValue == this.plantage) {
+            clearInterval(this.interval)
+         }
+      }, 1000);
+    }
+  }
+
+  changeProgressValue() {
+    if ( this.barValue) {
+      document.getElementById('progress-time').style.width = this.barValue.toString()+'%'
+    }
+  }
 }
