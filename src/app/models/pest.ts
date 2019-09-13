2383853f2003c1a887aab6cd87e3c2eecaf33970
AbiyHailu <abiyo02@yahoo.com>
2019-09-13 15:40:32 +0200

    2 files modified

--- src/app/models/pest.ts
+++ src/app/models/pest.ts
@@ -1,6 +1,11 @@
 export class Pest {
     id:number;
     name:string;
-    syptoms:string
+    syptoms:Array<string>
 }
 
+export const pests: Pest[] = [
+  { id: 1, name: 'Spider Mite', syptoms:["leaf blackness", "test syptom test"]},
+  { id: 2, name: 'Downy Mildew', syptoms:["leaf bottom", "test syptom test"]},
+  { id: 3, name: 'Aphides', syptoms:["leaf whitness", "test syptom test"]}, 
+];
--- src/app/models/pesticide.ts
+++ src/app/models/pesticide.ts
@@ -1,7 +1,14 @@
 export class pesticide {
     id:number;
     name:string;
-    purpose:Array<string>[];
+    purpose:Array<string>;
     efficency:number;//percentage
+    envImpact: number;
+    price:number
 }
 
+export const pesticides: pesticide[] = [
+  { id: 1, name: 'Selecron', purpose:["Spider Mite ", "Aphides"], efficency:50, envImpact:30, price:20},
+  { id: 2, name: 'testDowny' , purpose:["Downy"], efficency:70, envImpact:20, price:30},
+   { id: 2, name: 'test2Downy' , purpose:["Downy"], efficency:60 ,envImpact:20, price:20},
+];
